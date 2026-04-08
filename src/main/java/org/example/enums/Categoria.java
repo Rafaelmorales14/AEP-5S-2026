d67e27a5bac94cf157651cd771c81e7da6ecc952
Rafael Blasques Morales <168241944+Rafaelmorales14@users.noreply.github.com>
2026-04-08 13:13:19 -0300

    fix enums

--- src/main/java/org/example/enums/Categoria.java
+++
@@ -1,9 +0,0 @@
-package org.example.enums;
-
-public enum Categoria {
-    SOM_ALTO,
-    FESTA,
-    OBRA,
-    ANIMAL_BARULHENTO,
-    OUTROS;
-}
---
+++ src/main/java/org/example/model/enums/CategoriaSossego.java
@@ -0,0 +1,10 @@
+package org.example.model.enums;
+
+public enum CategoriaSossego {
+    VEICULO_SOM_ALTO,
+    FESTA_RESIDENCIAL,
+    OBRAS_IRREGULARES,
+    ANIMAL_BARULHENTO,
+    BAR_CASA_NOTURNA,
+    OUTROS;
+}
