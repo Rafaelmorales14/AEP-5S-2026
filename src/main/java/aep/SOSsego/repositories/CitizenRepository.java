bcabc26faea70dc856666371129bf9d296887086
Rafael Blasques Morales <rafaelblasques0611@gmail.com>
2026-06-01 20:56:14 -0300

    Create repositories citizen and user

---
+++ src/main/java/aep/SOSsego/repositories/CitizenRepository.java
@@ -0,0 +1,9 @@
+package aep.SOSsego.repositories;
+
+import aep.SOSsego.models.CitizenModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitizenRepository extends JpaRepository<CitizenModel, Long> {
+}
---
+++ src/main/java/aep/SOSsego/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package aep.SOSsego.repositories;
+
+import aep.SOSsego.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
