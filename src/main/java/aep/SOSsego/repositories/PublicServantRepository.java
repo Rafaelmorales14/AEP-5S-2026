d66550d57b62101a7c98088c33cb5e8da8c84668
Rafael Blasques Morales <rafaelblasques0611@gmail.com>
2026-06-02 12:37:29 -0300

    Create PublicServantRepository.java

---
+++ src/main/java/aep/SOSsego/repositories/PublicServantRepository.java
@@ -0,0 +1,9 @@
+package aep.SOSsego.repositories;
+
+import aep.SOSsego.models.PublicServantModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublicServantRepository extends JpaRepository<PublicServantModel, Long> {
+}
